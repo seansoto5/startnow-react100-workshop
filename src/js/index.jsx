a8fe5ac00a940c2342e1eea91424e061863b4e80
seansoto5 <sean_soto125@yahoo.com>
2018-08-30 21:28:06 -0700

    Restarted, left off on adding constructor to class 'SQUARE'

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => alert('click')}>
+        {this.props.value}
       </button>
     );
   }
@@ -13,7 +20,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
